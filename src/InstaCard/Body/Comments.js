2dd499dae231919377913fa9b6661e02bb8ae021
miho yamagata <miho.yamagata@gmail.com>
2020-11-03 21:42:48 +0900

    modify Comments

--- src/InstaCard/Body/Comments.js
+++ src/InstaCard/Body/Comments.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import Comment from './Comment';
+import PropTypes from 'prop-types';
 
-const Comments = () => (
-  <ul className="comments">
-    <Comment
-      username="testuser1"
-      comment="This is the insta-card!" />
-    <Comment
-      username="testuser2"
-      comment="So cool!" />
-  </ul>
-);
+const Comments = ({ data }) => {
+  const { commentOne, commentTwo } = data;
+  return(
+    <ul className="comments">
+      <Comment
+        username={commentOne.username}
+        comment={commentOne.usercomment} />
+      <Comment
+        username={commentTwo.username}
+        comment={commentTwo.usercomment} />
+    </ul>
+  );
+}
 
+Comments.propTypes = {
+  data: [
+    {
+      username: PropTypes.string.isRequired,
+      usercomment: PropTypes.string.isRequired
+    }
+  ]
+}
+
+Comments.defaultProps = {
+  data: {
+    commentOne: {
+      username: "testuser1",
+      usercomment: "This is the insta-card!"
+    },
+    commentTwo: {
+      username: "testuser2",
+      usercomment: "So cool!"
+    }
+  }
+}
 export default Comments;
